c5120aaff3860499ed960b920d72a8481625eafb
Tom Zurales <tomzurales@gmail.com>
2020-02-07 20:16:33 -0500

    Added basic error handling

--- environ.py
+++ environ.py
@@ -11,9 +11,17 @@ if len(args) == 1:
     exit()
 
 name = args[1]
-if name in keywords:
-    if name == keywords[0]:
-        print(f"Recording setup for {args[2]}\nPress ESC to end\n")
-        recordings[args[2]] = keyboard.record(until='esc')
-        keyboard.play(recordings[args[2]], speed_factor=10)
-print(f"Setting up environment {name}")
+if name == keywords[0]:
+    if len(args) != 3:
+        print("Please specify a name for this recording")
+        exit()
+    print(f"Recording setup for {args[2]}\nPress ESC to end\n")
+    recordings[args[2]] = keyboard.record(until='esc')
+    keyboard.play(recordings[args[2]], speed_factor=10)
+elif name == keywords[1]:
+    if len(args) != 3:
+        print("Please specify the name of the recording to delete")
+    printf(f"Deleting recording {args[2]}\n")
+    del recordings[args[2]]
+elif len(args) == 2:
+    print(f"Setting up environment {name}")
